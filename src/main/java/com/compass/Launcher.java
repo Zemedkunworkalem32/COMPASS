3fb4ca6a20f44c0b972f1b2b6acb0f8f10dd496f
Yoseph Asrat Kebede <yoseph.asrat@aastustudent.edu.et>
2026-05-28 00:03:10 +0300

    Delete src/main/java/com/compass/Launcher.java

--- src/main/java/com/compass/Launcher.java
+++
@@ -1,8 +0,0 @@
-// src/main/java/com/compass/Launcher.java
-package com.compass;
-
-public class Launcher {
-    public static void main(String[] args) {
-        CompassApplication.main(args);
-    }
-}
